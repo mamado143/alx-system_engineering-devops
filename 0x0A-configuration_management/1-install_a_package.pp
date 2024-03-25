8f77a77ab3c0e7fc3b5d562cde3c0505dba6573c
mamado143 <midodahir123@gmail.com>
2024-03-24 17:46:09 -0700

    fix install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,4 @@
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install_dependencies':
-  command => '/usr/bin/pip3 install Werkzeug==2.1.1',
-  path    => ['/usr/bin'],
-  unless  => '/usr/bin/pip3 show werkzeug | grep -q "Version: 2.1.1"',
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin'],
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
 }
