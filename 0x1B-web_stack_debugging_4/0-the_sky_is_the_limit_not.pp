6cb3ba9b1382a437d4d386b46e5278a6dda5c458
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2024-05-17 05:35:22 +0300

    add task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,9 @@
+#we got this file
+exec {'sets file limite for nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
