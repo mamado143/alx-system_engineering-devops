d5ca9c2770e9bb1d6e2542d62a994ad157d8219c
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2024-05-21 12:00:44 +0300

    update 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""
+Function to query subscribers on a given Reddit subreddit.
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """
+    Return "OK" if the subreddit exists, otherwise return 0.
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
-        try:
-            data = response.json().get("data")
-            if data:
-                return data.get("subscribers", 0)
-        except ValueError:
-            return 0
-    return 0
+        return "OK"  # Subreddit exists, return "OK"
+    else:
+        return 0
