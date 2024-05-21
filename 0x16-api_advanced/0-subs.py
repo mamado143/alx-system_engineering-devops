617a708d4a4de0299bf187b1aa0bd983a98c501d
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2024-05-21 11:17:13 +0300

    update 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -18,6 +18,8 @@ def number_of_subscribers(subreddit):
         return 0
 
 if __name__ == "__main__":
-    subreddit = input("Enter subreddit name: ")
-    subscribers = number_of_subscribers(subreddit)
-    print(subscribers)
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
