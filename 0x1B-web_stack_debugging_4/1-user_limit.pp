6f6637ac3fa77643cc8546212e741e3c83bca04d
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2024-05-22 13:20:52 +0300

    add task 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,7 @@
-#Enable the user holberton to login and open files without errors
-
-# Increase hard file limit for holberton user
-exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf'
-  path    => '/usr/local/bin/:/bin/'
+# Fixes upper/lower limit issue for user "holberton"
+exec { 'fix-os-config':
+  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command => "sed -ir 's/^holberton hard nofile 5$/holberton hard nofile 50000/g' /etc/security/limits.conf",
 }
-
-#Increase soft file limit for user holberton
-exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf'
-  path    => '/usr/local/bin/:/bin/'
-}
+exec { 'fix-os-config2':
+  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
