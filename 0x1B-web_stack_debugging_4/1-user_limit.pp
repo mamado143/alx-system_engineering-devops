1e128f2e0429aea5b302cc5220406f6f2e6ee0f9
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2024-05-22 14:10:22 +0300

    fix task 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,18 +1,13 @@
-# This enables holberton users to login and open files without errors
+#Enable the user holberton to login and open files without errors
 
-# Increase the hard file limit for the Holberton users
-
-exec { 'holberton-hard-file-fix':
-  command => "sed -i '/^holberton hard/s/5/50000/' /etc/security/limits.conf",
-  path    => '/usr/local/bin/:/bin/',
+# Increase hard file limit for holberton user
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf'
+  path    => '/usr/local/bin/:/bin/'
 }
 
-# Increase the soft file limit for the Holberton users
-
-exec { 'holberton-soft-file-fix':
-  command => 'sed -i "/^holberton soft/s/4/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/',
+#Increase soft file limit for user holberton
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf'
+  path    => '/usr/local/bin/:/bin/'
 }
-  command => 'sed -i "/^holberton soft/s/4/50000/" /etc/security/limits.conf',  path    => '/usr/local/bin/:/bin/',
-
-  
